70b9111d8739c35d1de4c5b141a6f73c649d480e
Kumar <amruthalurikumar.com>
2025-01-16 08:01:08 +0530

    setup2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import { Button } from "@/components/ui/button"
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-50">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button>Hefdfk</Button>
+    </div>
+    
   )
 }
 
 export default App
+
